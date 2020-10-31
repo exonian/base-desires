044e33b11185b5c578be58d0779d1e21e06a64d1
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-10-31 14:53:21 +0000

    Handle searches and exact matches
    Redirects to exact page when there's only one match
    Doesn't redirect to an exact match when there's multiple results
    But trailing slash enforces exact page
    Unfound exact page redirects back to search, and potentially from there
    back to the correct exact page

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,65 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useParams
+  useHistory,
+  useParams,
 } from 'react-router-dom';
 
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import { Warscrolls } from './warscrolls/data';
 import { toDisplay, toStandard } from './utils';
+import { exact } from 'prop-types';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/"><Page /></Route>
-        <Route path="/:slug" children={<Page />} />
+        <Route exact path="/"><Search /></Route>
+        <Route strict path="/:slug/" children={<Warscroll />} />
+        <Route path="/:slug" children={<Search />} />
       </Switch>
     </Router>
   )
 }
 
-const Page = () => {
+const Search = () => {
+  const history = useHistory()
   const { slug } = useParams()
-  const standardisedSlug = toStandard(slug)
-  const matches = Object.keys(Warscrolls).filter(name => toStandard(name).includes(standardisedSlug))
-  const pageName = toDisplay(slug)
+  const standardisedSlug = slug ? toStandard(slug) : ""
+  const matchNames = Object.keys(Warscrolls).filter(name => toStandard(name).includes(standardisedSlug))
+  // const exactMatch = matchNames.filter(name => toStandard(name) == standardisedSlug)
 
+  // if (exactMatch.length) history.push(`/${exactMatch}/`)
+  if (matchNames.length == 1) history.push(`/${toStandard(matchNames[0])}/`)
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{ pageName }</h1>
-        <p>{ matches }</p>
+      <header>
+        <h1>Warscrolls</h1>
+      </header>
+      <ul>
+        {matchNames.map((warscroll) =>
+          <li key={warscroll}>{warscroll}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+const Warscroll = () => {
+  const history = useHistory()
+  const { slug } = useParams()
+  const standardisedSlug = slug ? toStandard(slug) : ""
+  const exactMatch = Object.keys(Warscrolls).find(name => toStandard(name) == standardisedSlug)
+
+  return exactMatch ? (
+    <div className="App">
+      <header>
+        <h1>{ toDisplay(exactMatch) }</h1>
       </header>
     </div>
-  );
+  )
+  :
+  Search()
 }
 
 export default App;
