62e654b4de1a0f9887284421f37ed37b6dcd563d
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-10-31 21:51:34 +0000

    Make the importer handle the central data.ts too

--- import_data.py
+++ import_data.py
@@ -4,16 +4,20 @@ import glob
 import os
 
 if __name__ == "__main__":
+    SKIP_ALREADY_IMPORTED = False
     DATA_DIR = os.path.join(os.path.dirname(__file__), 'src', 'warscrolls', 'data')
+    DATA_FILE = os.path.join(os.path.dirname(__file__), 'src', 'warscrolls', 'data.ts')
 
     text_files = glob.glob(os.path.join(DATA_DIR, '*.txt'))
     print('Found the following source data files:')
     print(text_files)
     print('')
 
+    sizes = set()
+
     for text_file_path in text_files:
         ts_file_path = text_file_path.replace('.txt', '.ts')
-        if os.path.isfile(ts_file_path):
+        if SKIP_ALREADY_IMPORTED and os.path.isfile(ts_file_path):
             print ("{} already exists: skipping".format(ts_file_path))
             continue
 
@@ -21,10 +25,21 @@ if __name__ == "__main__":
         with open(text_file_path, 'r') as f:
             for line in f:
                 words = line.split()
-                if words[-2] == "x":
-                    warscrolls.append((' '.join(words[:-3]), ' '.join(words[-3:])))
+                if words[-1][-1] == ")":
+                    opening_bracket = [words.index(word) for word in words if word.startswith('(')][-1]
+                    name = ' '.join(words[:opening_bracket])
+                    size = ' '.join(words[opening_bracket:])
+                if len(words) >=2 and words[-2] == "x":
+                    name = ' '.join(words[:-3])
+                    size = ' '.join(words[-3:])
+                elif words[-2:] == ["Use", "model"]:
+                    name = ' '.join(words[:-2])
+                    size = ' '.join(words[-2:])
                 else:
-                    warscrolls.append((' '.join(words[:-1]), words[-1]))
+                    name = ' '.join(words[:-1])
+                    size = words[-1]
+                warscrolls.append((name, size))
+                sizes.update([size])
 
         with open(ts_file_path, 'w') as f:
             f.write("""
@@ -40,3 +55,23 @@ const Warscrolls: TWarscrolls = {""")
 }
 export default Warscrolls""")
             print ("{} created".format(ts_file_path))
+
+    ts_files = glob.glob(os.path.join(DATA_DIR, '*.ts'))
+    module_names = sorted([os.path.relpath(f, DATA_DIR).split(".ts")[0] for f in ts_files])
+    print('')
+    print('Updating combined data file to import the following typescript data files:')
+    print(', '.join(module_names))
+    print('')
+    with open(DATA_FILE, 'w') as f:
+        f.write("""import { TWarscrolls } from "./types";\n""")
+        for module in module_names:
+            f.write('''import {module_title}Warscrolls from "./data/{module}"\n'''.format(module=module, module_title=module.title()))
+        f.write("\n")
+        f.write("export const Warscrolls: TWarscrolls = {\n")
+        for module in module_names:
+            f.write("    ...{module_title}Warscrolls,\n".format(module=module, module_title=module.title()))
+        f.write("}")
+
+    print('The following base sizes were detected:')
+    for size in sizes:
+        print('''| "{}"'''.format(size))
--- src/warscrolls/data.ts
+++ src/warscrolls/data.ts
@@ -1,6 +1,28 @@
 import { TWarscrolls } from "./types";
+import Beasts_Of_ChaosWarscrolls from "./data/beasts_of_chaos"
+import Blades_Of_KhorneWarscrolls from "./data/blades_of_khorne"
+import Daemons_Of_ChaosWarscrolls from "./data/daemons_of_chaos"
+import Disciples_Of_TzeentchWarscrolls from "./data/disciples_of_tzeentch"
+import Hedonites_Of_SlaaneshWarscrolls from "./data/hedonites_of_slaanesh"
+import Legions_Of_AzgorhWarscrolls from "./data/legions_of_azgorh"
+import Maggotkin_Of_NurgleWarscrolls from "./data/maggotkin_of_nurgle"
+import Monsters_Of_ChaosWarscrolls from "./data/monsters_of_chaos"
+import SkavenWarscrolls from "./data/skaven"
+import Slaves_To_DarknessWarscrolls from "./data/slaves_to_darkness"
 import StormcastWarscrolls from "./data/stormcast"
+import Tamurkhans_HordeWarscrolls from "./data/tamurkhans_horde"
 
 export const Warscrolls: TWarscrolls = {
+    ...Beasts_Of_ChaosWarscrolls,
+    ...Blades_Of_KhorneWarscrolls,
+    ...Daemons_Of_ChaosWarscrolls,
+    ...Disciples_Of_TzeentchWarscrolls,
+    ...Hedonites_Of_SlaaneshWarscrolls,
+    ...Legions_Of_AzgorhWarscrolls,
+    ...Maggotkin_Of_NurgleWarscrolls,
+    ...Monsters_Of_ChaosWarscrolls,
+    ...SkavenWarscrolls,
+    ...Slaves_To_DarknessWarscrolls,
     ...StormcastWarscrolls,
+    ...Tamurkhans_HordeWarscrolls,
 }
