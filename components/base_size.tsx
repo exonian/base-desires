03334fef2a651759a002aacefaad19bda0667256
Michael Blatherwick <michael@michaelblatherwick.co.uk>
2024-02-17 21:22:09 +0000

    Make base sizes on cards into links

---
+++ components/base_size.tsx
@@ -0,0 +1,49 @@
+import { zip } from 'lodash';
+import Link from 'next/link';
+import React from 'react';
+import { toStandard } from '../utils/text';
+
+interface IBaseSizeProps {
+  baseSize: string
+}
+
+type TPart = {
+  text: string
+  element: 'link' | 'text' | 'small'
+}
+
+export const BaseSize: React.FC<IBaseSizeProps> = props => {
+  const { baseSize } = props
+  const baseSizeParts: TPart[] = baseSize.split(', ').reduce((accum, text, index) => {
+    if (index > 0) accum.push({ text: ', ', element: 'text' })
+
+    const square_brace_position = text.indexOf('[')
+    if (square_brace_position > -1) {
+        accum.push({ text: text.slice(0, square_brace_position).trim(), element: 'link' })
+        accum.push({ text: ' ' + text.slice(square_brace_position).trim(), element: 'small' })
+    }
+    else accum.push({ text: text, element: 'link' })
+
+    return accum
+  }, [] as TPart[])
+
+  return (
+    <p className="card-base-size">
+      {baseSizeParts.map((part, index) =>
+        <React.Fragment key={index}>
+          {part.element === 'link' ? (
+            <Link href={`/?s=${toStandard(part.text)}`}>{part.text}</Link>
+          ) : (
+            <>
+              {part.element === 'text' ? (
+                <span>{part.text}</span>
+              ) : (
+                <small>{part.text}</small>
+              )}
+            </>
+          )}
+        </React.Fragment>
+      )}
+    </p>
+  )
+}
--- components/card.tsx
+++ components/card.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import { TWarscroll } from '../warscrolls/types';
 import { toDisplay, toStandard } from '../utils/text';
+import { BaseSize } from './base_size';
 
 interface ICardProps {
   name: string
@@ -18,7 +19,7 @@ export const Card: React.FC<ICardProps> = props => {
     <div className="card warscroll-card mb-3">
       <div className="card-body">
         <h2>{ link ? <Link href={`/${toStandard(name)}/`}>{name}</Link> : <>{name}</>}</h2>
-        <p className="card-text">{ warscroll.baseSize }</p>
+        <BaseSize baseSize={ warscroll.baseSize } />
         { warscroll.notes && <p className="card-text card-notes">{ warscroll.notes }</p>}
       </div>
       <div className="card-footer">
