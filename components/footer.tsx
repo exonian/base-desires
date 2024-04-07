199cb53a90b619b2a3704ea3c0456ee5b9b9b65e
Michael Blatherwick <michael@michaelblatherwick.co.uk>
2024-04-07 18:12:58 +0100

    Add footer links between the two versions of the site

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import { FaGithub, FaTwitter } from 'react-icons/fa'
+import { game } from '../utils/env'
 
 export const Footer: React.FC = () => {
   return (
@@ -6,6 +8,11 @@ export const Footer: React.FC = () => {
       <p>By Michael Blatherwick
         <a className="social-link" href="https://twitter.com/rogue_michael"><FaTwitter /> @rogue_michael</a>
         <a className="social-link" href="https://github.com/exonian"><FaGithub /> exonian</a>
+        { game === 'tow' ?
+          <Link className="social-link" href="https://basedesires.com">Base Desires</Link>
+        :
+          <Link className="social-link"  href="https://tow.basedesires.com">Base Desires²</Link>
+        }
       </p>
       <blockquote className="blockquote pr-3">
         <p className="mb-0">&quot;basedesires.com. Haha can you imagine if that was a website about Warhammer bases…&quot;</p>
