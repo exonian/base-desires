1f36edcf5b4a0dab4f0d7463ae258624f2b806c3
Michael Blatherwick <michael@michaelblatherwick.co.uk>
2024-06-03 18:32:37 +0100

    Make "and" lowercase
    Like we already do with "of"
    Simple string matching of " And " to make sure we don't do it within
    words

--- utils/text.ts
+++ utils/text.ts
@@ -1,6 +1,6 @@
 import { startCase, camelCase } from "lodash";
 
-export const toDisplay = (val: string): string => startCase(camelCase(val)).replace(" Of ", " of ")
+export const toDisplay = (val: string): string => startCase(camelCase(val)).replace(" Of ", " of ").replace(" And ", " and ")
 
 export const toStandard = (val: string): string => val.toLowerCase().split(' ').join('-')
 
