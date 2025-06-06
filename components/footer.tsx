678172e80f72a6e4d1f37055a6b412a8affbb223
Michael Blatherwick <michael@michaelblatherwick.co.uk>
2025-06-06 23:40:02 +0100

    Update footer links between site variants

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,11 +7,9 @@ export const Footer: React.FC = () => {
     <footer className="footer text-center">
       <p>By Michael Blatherwick
         <a className="social-link" href="https://github.com/exonian"><FaGithub /> exonian</a>
-        { game === 'tow' ?
-          <Link className="social-link" href="https://basedesires.com">Base Desires</Link>
-        :
-          <Link className="social-link"  href="https://tow.basedesires.com">Base Desires²</Link>
-        }
+        { game !== 'aos' && <Link className="social-link" href="https://basedesires.com">Base Desires</Link> }
+        { game !== 'tow' && <Link className="social-link"  href="https://tow.basedesires.com">Base Desires²</Link> }
+        { game !== '40k' && <Link className="social-link" href="https://40k.basedesires.com">Space Desires</Link> }
       </p>
       <blockquote className="blockquote pr-3">
         <p className="mb-0">&quot;basedesires.com. Haha can you imagine if that was a website about Warhammer bases…&quot;</p>
