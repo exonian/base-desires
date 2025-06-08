14dd2edb6fe2fa422605720e815954a14fa5c680
Michael Blatherwick <michael@michaelblatherwick.co.uk>
2025-06-08 10:07:55 +0100

    Add new AoS pdf
    No change in data, but update the source name

--- utils/env.ts
+++ utils/env.ts
@@ -45,8 +45,8 @@ export const dataDirectory = path.join(process.cwd(), 'data', gameDirectory)
 export const profilesDirectory = path.join(dataDirectory, 'profiles')
 
 export const aosSources :any = {
-    'profiles': 'Battle Profiles April (End) 2025',
-    'legends': 'Legends April (End) 2025',
+    'profiles': 'Battle Profiles May 2025',
+    'legends': 'Legends May 2025',
     'unlisted': 'Unlisted',
     'corrected': 'Unofficial correction',
 }
